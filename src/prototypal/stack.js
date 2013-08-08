ce31f7df12d368f971bfeeb6fba3ed3ada9bd238
Hack Reactor Students <github@hackreactor.com>
2013-08-07 22:27:56 -0700

    prototypal inheritance passes stackspec with Object.create

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,25 @@
 var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var makeStack = Object.create(Stack.prototype);
+  this.size = 0;
+  this.storage = {};
+  return makeStack;
 };
+  Stack.prototype = {};
+
+  Stack.prototype.push = function(value) {
+    storage[size++] = value;
+  };
+
+  Stack.prototype.pop = function() {
+    if(size) {
+      var temp = storage[size - 1];
+      delete storage[size - 1];
+      size--;
+      return temp;
+    }
+  };
+
+  Stack.prototype.size = function() {
+    return size;
+  };
+
